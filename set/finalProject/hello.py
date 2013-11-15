589e8396173c99773373fd95e9d2f8e725e12eb1
sdhaivat2011 <sdhaivat2011@gmail.com>
2013-11-15 05:21:50 +0000

    adding basic files

--- set/finalProject/hello.py
+++ set/finalProject/hello.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask
+
+# initialization
 app = Flask(__name__)
+app.config.update(
+		    DEBUG = True,
+		    )
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+# controllers
+@app.route("/")
+def hello():
+	return "Hello from Python!"
 
-if __name__ == '__main__':
-	app.run()
+# launch
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
